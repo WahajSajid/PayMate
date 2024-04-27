16f59d05b350f1a3281b24d46f681854f54a49b5
Wahaj Sajid <mwahajsajidali@gmail.com>
2024-04-27 19:33:57 +0500

    Initial commit

---
+++ app/src/main/java/com/application/paymate/CreateNetworkOrLogIn.kt
@@ -0,0 +1,21 @@
+package com.application.paymate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.application.paymate.databinding.FragmentCreateNetworkOrLogInBinding
+
+class CreateNetworkOrLogIn : Fragment() {
+    lateinit var binding:FragmentCreateNetworkOrLogInBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_create_network_or_log_in, container, false)
+        return binding.root
+    }
+}
